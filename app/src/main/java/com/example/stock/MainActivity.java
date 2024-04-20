b97b6ee3ee495acd08b511d94af1854fe0d8024c
kapiekp <pandeyk@usc.edu>
2024-04-19 17:26:27 -0700

    started

---
+++ app/src/main/java/com/example/stock/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.stock;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Locale;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        String date_n = new SimpleDateFormat("dd MMMM yyyy", Locale.getDefault()).format(new Date());
+        TextView date  = (TextView) findViewById(R.id.curdate);
+        date.setText(date_n);
+
+
+    }
+}
